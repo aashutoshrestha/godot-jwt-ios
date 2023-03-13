5e8cd1722574a03cd6385dec51e777c260de37b9
Aashutosh Shrestha <aashutosh@teslatech.com.np>
2023-03-13 10:44:33 +0545

    First Commit

---
+++ godot-jwt/godot_jwt.h
@@ -0,0 +1,12 @@
+//
+//  godot_jwt.h
+//  godot-jwt
+//
+//  Created by Aashutosh Shrestha on 3/13/23.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface godot_jwt : NSObject
+
+@end
---
+++ godot-jwt/godot_jwt.mm
@@ -0,0 +1,12 @@
+//
+//  godot_jwt.m
+//  godot-jwt
+//
+//  Created by Aashutosh Shrestha on 3/13/23.
+//
+
+#import "godot_jwt.h"
+
+@implementation godot_jwt
+
+@end
